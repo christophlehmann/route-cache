1e39b37f2b94d495d6a4682b114049c0b2caba0c
Christoph Lehmann <christoph.lehmann@networkteam.com>
2024-11-04 23:22:30 +0100

    Populate cache entries only in FE

--- Classes/EventListener/AfterRecordLanguageOverlayEventListener.php
+++ Classes/EventListener/AfterRecordLanguageOverlayEventListener.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace Lemming\RouteCache\EventListener;
 
 use Lemming\RouteCache\Factory\CacheIdentifierFactory;
+use Psr\Http\Message\ServerRequestInterface;
 use TYPO3\CMS\Core\Cache\CacheManager;
 use TYPO3\CMS\Core\Cache\Frontend\FrontendInterface;
+use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Core\Domain\Event\AfterRecordLanguageOverlayEvent;
+use TYPO3\CMS\Core\Http\ApplicationType;
 
 final class AfterRecordLanguageOverlayEventListener
 {
@@ -21,7 +24,7 @@ final class AfterRecordLanguageOverlayEventListener
     public function populateSlugCacheEntry(AfterRecordLanguageOverlayEvent $event): void
     {
         $table = $event->getTable();
-        if ($table !== 'tt_content') {
+        if ($this->isFrontendRequest() && $table !== 'tt_content') {
             $slugFieldName = $this->getSlugFieldName($table);
             if ($slugFieldName) {
                 $languageAspect = $event->getLanguageAspect();
@@ -40,5 +43,12 @@ final class AfterRecordLanguageOverlayEventListener
             }
         }
         return null;
+    }
+
+    protected function isFrontendRequest(): bool
+    {
+        return !Environment::isCli()
+            && ($GLOBALS['TYPO3_REQUEST'] ?? null) instanceof ServerRequestInterface
+            && ApplicationType::fromRequest($GLOBALS['TYPO3_REQUEST'])->isFrontend();
     }
 }
